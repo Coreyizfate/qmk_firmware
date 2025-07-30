8cb73853b2f079e14b167b9e995f1d86466018c2
coreyizfate <coreymichael913@gmail.com>
2025-07-30 13:21:20 -0500

    adding rules.mk to working keymap folder

--- keyboards/keychron/v1/ansi/keymaps/working/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/working/keymap.c
@@ -29,14 +29,14 @@ enum layers{
 #define KC_FLXP LGUI(KC_E)
 
 // Home Row Mods
-#define HR_LALT LALT(KC_A)
-#define HR_LCTL LCTL(KC_S)
-#define HR_LSFT LSFT(KC_D)
-#define HR_LGUI LGUI(KC_F)
-#define HR_RALT RALT(KC_SCLN)
-#define HR_RCTL RCTL(KC_L)
-#define HR_RSFT RSFT(KC_K)
-#define HR_RGUI RGUI(KC_J)
+#define HR_LALT LALT_T(KC_A)
+#define HR_LCTL LCTL_T(KC_S)
+#define HR_LSFT LSFT_T(KC_D)
+#define HR_LGUI LGUI_T(KC_F)
+#define HR_RALT RALT_T(KC_SCLN)
+#define HR_RCTL RCTL_T(KC_L)
+#define HR_RSFT RSFT_T(KC_K)
+#define HR_RGUI RGUI_T(KC_J)
 
 #define BL_SPCL LT(_SPCL, KC_SPC)
 
@@ -52,21 +52,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SPCL] = LAYOUT_ansi_82(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  LALT(KC_F4),  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,              _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_LEAD,  _______,  _______,  KC_LEAD,              _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
+
+    [_ALTL] = LAYOUT_ansi_82(
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,              _______,            _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  KC_LEAD,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 
-    [_ALTL] = LAYOUT_ansi_82(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_INS,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_FL] = LAYOUT_ansi_82(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
+        QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
@@ -85,7 +86,7 @@ void leader_end_user(void) {
     } else if (leader_sequence_two_keys(KC_V)) {
         // Leader, d, d => Ctrl+A, Ctrl+C
         SEND_STRING(MEH(KC_V));
-    } else if (leader_sequence_three_keys(KC_O)) {}
+    } else if (leader_sequence_three_keys(KC_O)) {
         // Leader, d, d, s => Types the below string
         SEND_STRING((LCG(KC_O)));
     } else if (leader_sequence_two_keys(KC_R)) {
---
+++ keyboards/keychron/v1/ansi/keymaps/working/rules.mk
@@ -0,0 +1,10 @@
+# custom matrix setup
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
+
+VIA_ENABLE = yes
+
+LEADER_ENABLED = yes
+LEADER_TIMEOUT = 300
+LEADER_PER_KEY_TIMING = yes
